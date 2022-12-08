5a0f009a1abb6f20c3d77a64f6a79e986a195bc2
Alexander <dr.ambercat@gmail.com>
2022-12-08 19:24:06 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/transform-runtime'],
+          },
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+      favicon: 'src/favicon.ico',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+}
